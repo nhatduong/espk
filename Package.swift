3e81b83d2e47b3f93229ee76b3623b79500b508d
nhatduong <nhatbg89@gmail.com>
2025-02-17 15:44:28 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "espeak-ng",
   products: [
-    .library(name: "libespeak_ng", targets: ["libespeak-ng"]),
+    .library(name: "libespeak-ng", targets: ["libespeak-ng"]),
     .library(name: "espeak-ng-data", targets: ["data"]),
   ],
   targets: [
@@ -30,7 +30,7 @@ let package = Package(
       ]
     ),
     .target(
-      name: "libespeak_ng",
+      name: "libespeak-ng",
       dependencies: [ "libsonic", "libucd" ],
       exclude: [
         "_repo",
